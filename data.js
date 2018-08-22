31f4030ff73a4859ed157146eb8a4d2de35e3a8e
mingfun wong <mingfun.wong.chn@gmail.com>
2018-08-22 20:49:34 +0900

    add shensi

--- data.js
+++ data.js
@@ -27,6 +27,23 @@ var data = {
       ]
     },
     {
+      icon: "fas fa-child",
+      name: "周边站点",
+      child: [
+        {name: "神思导航", desc: "EOS 生态工具和资讯导", url: "https://www.shensi.com/", img: "thumb/shensi.com.jpg"},
+        {name: "IMEOS.ONE", desc: "EOS 生态资讯先锋门户", url: "https://imeos.one/", img: "thumb/imeos.one.png"},
+        {name: "EOS Channel", desc: "专注于 EOS 资讯与技术", url: "http://www.eoschannel.com/", img: "thumb/eoschannel.com.jpg"},
+        {name: "EOS 原力区", desc: "技术社区", url: "https://eostalk.one/", img: "thumb/eostalk.one.jpg"},
+        {name: "EOSindex", desc: "基于 EOS 的项目目录", url: "https://eosindex.io/", img: "thumb/eosindex.io.jpg"},
+        {name: "EOS Tracker", desc: "区块浏览器", url: "http://eostracker.io/", img: "thumb/eostracker.io.jpg"},
+        {name: "EOS.TOP", desc: "EOS 区块链社区", url: "https://eos.top/", img: "thumb/eos.top.jpg"},
+        {name: "引力区", desc: "区块链生态社群", url: "http://eosfans.one/", img: "thumb/eosfans.one.jpg"},
+        {name: "EOS Drops", desc: "EOS 糖果空投列表", url: "https://www.eosdrops.io/", img: "thumb/eosdrops.io.jpg"},
+        {name: "币问", desc: "区块链第一知识站", url: "https://www.bitask.org/topic/EOS", img: "thumb/bitask.org!topic!EOS.jpg"},
+        {name: "币乎", desc: "垂直社区平台", url: "https://bihu.com/?category=hots&code=EOS", img: "https://bihu.com/static/media/logo.81b9b93e.png"},
+      ]
+    },
+    {
       icon: "fab fa-weixin",
       name: "公众号",
       child: [
@@ -39,23 +56,6 @@ var data = {
       name: "EOS 应用",
       child: [
         {name: "EOSfinex", desc: "高性能分散式交易所", url: "https://eosfinex.com/", img: "thumb/eosfinex.com.jpg"},
-        {name: "Cybex", desc: "去中心化交易所", url: "https://cybex.io/", img: "thumb/cybex.io!index_zh.html.jpg"},
-      ]
-    },
-    {
-      icon: "fas fa-child",
-      name: "周边站点",
-      child: [
-        {name: "IMEOS.ONE", desc: "EOS 生态资讯先锋门户", url: "https://imeos.one/", img: "thumb/imeos.one.png"},
-        {name: "EOS Channel", desc: "专注于 EOS 资讯与技术", url: "http://www.eoschannel.com/", img: "thumb/eoschannel.com.jpg"},
-        {name: "EOS 原力区", desc: "技术社区", url: "https://eostalk.one/", img: "thumb/eostalk.one.jpg"},
-        {name: "EOSindex", desc: "基于 EOS 的项目目录", url: "https://eosindex.io/", img: "thumb/eosindex.io.jpg"},
-        {name: "EOS Tracker", desc: "区块浏览器", url: "http://eostracker.io/", img: "thumb/eostracker.io.jpg"},
-        {name: "EOS.TOP", desc: "EOS 区块链社区", url: "https://eos.top/", img: "thumb/eos.top.jpg"},
-        {name: "引力区", desc: "区块链生态社群", url: "http://eosfans.one/", img: "thumb/eosfans.one.jpg"},
-        {name: "EOS Drops", desc: "EOS 糖果空投列表", url: "https://www.eosdrops.io/", img: "thumb/eosdrops.io.jpg"},
-        {name: "币问", desc: "区块链第一知识站", url: "https://www.bitask.org/topic/EOS", img: "thumb/bitask.org!topic!EOS.jpg"},
-        {name: "币乎", desc: "垂直社区平台", url: "https://bihu.com/?category=hots&code=EOS", img: "https://bihu.com/static/media/logo.81b9b93e.png"},
       ]
     }
   ]
