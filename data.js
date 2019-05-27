fea737627b7606fa519849b9ea5a26408f1f51dc
mingfun wong <mingfun.wong.chn@gmail.com>
2019-05-28 00:04:40 +0900

    add data

--- data.js
+++ data.js
@@ -18,9 +18,7 @@ var data = {
         {name: "EOS", desc: "官方网站", url: "https://eos.io/", img: "thumb/eos.io.jpg"},
         {name: "GitHub", desc: "源代码", url: "https://github.com/EOSIO/eos", img: "thumb/github.com!EOSIO!eos.jpg"},
         {name: "Github Milestone", desc: "开发进度", url: "https://github.com/EOSIO/eos/milestones", img: "thumb/github.com!EOSIO!eos!milestones.jpg"},
-        {name: "Documentation", desc: "开发手册", url: "https://eosio.github.io/eos/", img: "thumb/eosio.github.io!eos.jpg"},
         {name: "Twitter", desc: "推特", url: "https://twitter.com/eos_io", img: "thumb/twitter.com!eos_io.jpg"},
-        {name: "Facebook", desc: "脸书", url: "https://www.facebook.com/eosblockchain", img: "thumb/facebook.com!eosblockchain.jpg"},
         {name: "Telegram", desc: "电报", url: "https://eos.io/chat/", img: "thumb/eos.io!chat.jpg"},
         {name: "Medium", desc: "Medium", url: "https://medium.com/@eosio", img: "thumb/medium.com!@eosio.jpg"},
         {name: "Steemit", desc: "Steemit", url: "https://steemit.com/trending/eos", img: "thumb/steemit.com!trending!eos.jpg"},
@@ -30,10 +28,11 @@ var data = {
       icon: "fas fa-child",
       name: "周边站点",
       child: [
+        {name: "公鹿钱包", desc: "去中心化应用生态服务的数字钱包", url: "https://www.buckwallet.vip/", img: "thumb/buckwallet.png"},
+        {name: "方块狗", desc: "为开发者提供更强大的基础服务", url: "https://www.blockdog.com/", img: "thumb/blockdog.png"},
         {name: "神思导航", desc: "EOS 生态工具和资讯导航", url: "https://www.shensi.com/", img: "thumb/shensi.com.jpg"},
         {name: "IMEOS.ONE", desc: "EOS 生态资讯先锋门户", url: "https://imeos.one/", img: "thumb/imeos.one.png"},
         {name: "EOS Channel", desc: "专注于 EOS 资讯与技术", url: "http://www.eoschannel.com/", img: "thumb/eoschannel.com.jpg"},
-        {name: "EOS 原力区", desc: "技术社区", url: "https://eostalk.one/", img: "thumb/eostalk.one.jpg"},
         {name: "EOSindex", desc: "基于 EOS 的项目目录", url: "https://eosindex.io/", img: "thumb/eosindex.io.jpg"},
         {name: "EOS Tracker", desc: "区块浏览器", url: "http://eostracker.io/", img: "thumb/eostracker.io.jpg"},
         {name: "EOS.TOP", desc: "EOS 区块链社区", url: "https://eos.top/", img: "thumb/eos.top.jpg"},
@@ -48,13 +47,6 @@ var data = {
       child: [
         {name: "EOS 引力区", desc: "中国第一大社区", url: "https://open.weixin.qq.com/qr/code?username=eosgravity", img: "https://open.weixin.qq.com/qr/code?username=eosgravity"},
         {name: "EOS 技术爱好者", desc: "技术爱好者", url: "https://open.weixin.qq.com/qr/code?username=EOSTechLover", img: "https://open.weixin.qq.com/qr/code?username=EOSTechLover"},
-      ]
-    },
-    {
-      icon: "fab fa-stack-exchange",
-      name: "EOS 应用",
-      child: [
-        {name: "EOSfinex", desc: "高性能分散式交易所", url: "https://eosfinex.com/", img: "thumb/eosfinex.com.jpg"},
       ]
     }
   ]
